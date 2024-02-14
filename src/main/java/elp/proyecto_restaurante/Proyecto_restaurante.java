3ced32eaa1aa52904af45f54b2f85d7f0f3e41c9
Elias <leblicqe@gmail.com>
2024-02-13 20:55:26 -0600

    inicio vacio

---
+++ src/main/java/elp/proyecto_restaurante/Proyecto_restaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package elp.proyecto_restaurante;
+
+/**
+ *
+ * @author elp51
+ */
+public class Proyecto_restaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
